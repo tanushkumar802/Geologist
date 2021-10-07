e5aed1d50fd1286b70e21250120a531bd5a63edd
tanushkumar802 <84520701+tanushkumar802@users.noreply.github.com>
2021-10-07 11:02:35 +0530

    Add files via upload

---
+++ Rubber.js
@@ -0,0 +1,10 @@
+class Rubber{
+    constructor(x,y){
+        var options ={
+restitution:0.3,
+friction:0.5,
+density:1
+        }
+    }
+   
+}
---
+++ hammer.js
@@ -0,0 +1,9 @@
+class Hammer{
+constructor(x,y){
+    var options = {
+        'density':2,
+        'friction':1.0,
+        'restitution': 0.5
+    };
+}
+}
